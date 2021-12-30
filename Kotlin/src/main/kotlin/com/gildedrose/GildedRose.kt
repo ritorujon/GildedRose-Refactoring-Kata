0c9c068245ae878b6e4f64cf7f135154ded139ab
Jan Ondrich <jan.ondrich.co@gmail.com>
2021-12-30 02:40:20 +0100

    kotlin - replaced "if" statements with "when"

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,28 +7,33 @@ class GildedRose(var items: Array<Item>) {
     }
 
     private fun updateItemQuality(item: Item) {
-        if (item.name == "Sulfuras, Hand of Ragnaros") return
-
-        if (item.name == "Aged Brie") {
-            item.increaseQuality(1)
-            if (item.sellIn <= 0) {
+        when (item.name) {
+            "Sulfuras, Hand of Ragnaros" -> {
+                return
+            }
+            "Aged Brie" -> {
                 item.increaseQuality(1)
+                if (item.sellIn <= 0) {
+                    item.increaseQuality(1)
+                }
             }
-        } else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            item.increaseQuality(1)
-            if (item.sellIn < 11) {
+            "Backstage passes to a TAFKAL80ETC concert" -> {
                 item.increaseQuality(1)
+                if (item.sellIn < 11) {
+                    item.increaseQuality(1)
+                }
+                if (item.sellIn < 6) {
+                    item.increaseQuality(1)
+                }
+                if (item.sellIn <= 0) {
+                    item.quality = 0
+                }
             }
-            if (item.sellIn < 6) {
-                item.increaseQuality(1)
-            }
-            if (item.sellIn <= 0) {
-                item.quality = 0
-            }
-        } else {
-            item.decreaseQuality(1)
-            if (item.sellIn <= 0) {
+            else -> {
                 item.decreaseQuality(1)
+                if (item.sellIn <= 0) {
+                    item.decreaseQuality(1)
+                }
             }
         }
         item.sellIn--
