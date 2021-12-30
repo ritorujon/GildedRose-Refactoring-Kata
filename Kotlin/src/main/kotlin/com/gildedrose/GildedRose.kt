93b0962cbf95a67aa8c57a87f161c9c55bcfcbea
Jan Ondrich <jan.ondrich.co@gmail.com>
2021-12-30 01:41:21 +0100

    kotlin - improved readability of setting the quality and sellIn

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -10,41 +10,39 @@ class GildedRose(var items: Array<Item>) {
         if (item.name == "Sulfuras, Hand of Ragnaros") return
         if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
             if (item.quality < 50) {
-                item.quality = item.quality + 1
-
+                item.quality++
                 if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality++
                         }
                     }
-
                     if (item.sellIn < 6) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1
+                            item.quality++
                         }
                     }
                 }
             }
         } else {
             if (item.quality > 0) {
-                item.quality = item.quality - 1
+                item.quality--
             }
         }
 
-        item.sellIn = item.sellIn - 1
+        item.sellIn--
 
         if (item.sellIn < 0) {
             if (item.name == "Aged Brie") {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality++
                 }
             } else {
                 if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    item.quality = item.quality - item.quality
+                    item.quality = 0
                 } else {
                     if (item.quality > 0) {
-                        item.quality = item.quality - 1
+                        item.quality--
                     }
                 }
             }
