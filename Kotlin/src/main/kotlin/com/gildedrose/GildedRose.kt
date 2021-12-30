e115d1e3d25de9f6b7904a34583cf7fd339993d7
Jan Ondrich <jan.ondrich.co@gmail.com>
2021-12-30 01:20:22 +0100

    kotlin - invert conditions to see more clearly which items are processed where

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,13 +7,7 @@ class GildedRose(var items: Array<Item>) {
     }
 
     private fun updateItemQuality(item: Item) {
-        if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-                if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.quality = item.quality - 1
-                }
-            }
-        } else {
+        if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
             if (item.quality < 50) {
                 item.quality = item.quality + 1
 
@@ -31,6 +25,12 @@ class GildedRose(var items: Array<Item>) {
                     }
                 }
             }
+        } else {
+            if (item.quality > 0) {
+                if (item.name != "Sulfuras, Hand of Ragnaros") {
+                    item.quality = item.quality - 1
+                }
+            }
         }
 
         if (item.name != "Sulfuras, Hand of Ragnaros") {
@@ -38,19 +38,19 @@ class GildedRose(var items: Array<Item>) {
         }
 
         if (item.sellIn < 0) {
-            if (item.name != "Aged Brie") {
-                if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.name == "Aged Brie") {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            } else {
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    item.quality = item.quality - item.quality
+                } else {
                     if (item.quality > 0) {
                         if (item.name != "Sulfuras, Hand of Ragnaros") {
                             item.quality = item.quality - 1
                         }
                     }
-                } else {
-                    item.quality = item.quality - item.quality
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
                 }
             }
         }
